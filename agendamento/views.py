a4bb621a553a38985827f75013fc9ebb4685b3ec
MariaLuizaMB <mariamadeirabm2@gmail.com>
2025-10-16 20:07:51 -0300

    Update files

--- agendamento/views.py
+++ agendamento/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+
+@login_required
 def home(request):
     return render(request, 'index.html')

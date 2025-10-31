4e7484085306abf9ee2e7cda7fec1f1630ea909b
MariaLuizaMB <mariamadeirabm2@gmail.com>
2025-10-31 03:03:51 -0300

    Update file

--- agendamento/admin.py
+++ agendamento/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Usuario, Sala, Agendamento, AgendamentoUsuario
 
 
 @admin.register(Usuario)
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('username','email', 'tipo_usuario', 'cargo', 'is_active')
-    search_fields = ('username', 'email')
-    list_filter = ('tipo_usuario', 'cargo', 'is_active')
+class UsuarioAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Informações Pessoais', {'fields': ('username', 'first_name', 'last_name', 'cargo', 'tipo_usuario')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'cargo', 'tipo_usuario', 'is_staff', 'is_active')}
+        ),
+    )
+    list_display = ('email', 'username', 'tipo_usuario', 'is_staff')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
 
 
 @admin.register(Sala)
@@ -14,6 +27,11 @@ class SalaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'tipo_sala', 'capacidade', 'criador')
     search_fields = ('nome',)
     list_filter = ('tipo_sala',)
+    
+class AgendamentoUsuarioInline(admin.TabularInline):
+    """Permite adicionar vários usuários dentro do agendamento."""
+    model = AgendamentoUsuario
+    extra = 1
 
 
 @admin.register(Agendamento)
@@ -21,9 +39,11 @@ class AgendamentoAdmin(admin.ModelAdmin):
     list_display = ('codigo_agendamento', 'nome', 'sala', 'data', 'hora_inicio', 'hora_fim', 'status', 'criador')
     list_filter = ('status', 'sala', 'data')
     search_fields = ('codigo_agendamento', 'nome', 'sala__nome', 'criador__username')
+    inlines = [AgendamentoUsuarioInline]
     ordering = ('-data', '-hora_inicio')
     date_hierarchy = 'data'
     readonly_fields = ('codigo_agendamento',)
+
 
 @admin.register(AgendamentoUsuario)
 class AgendamentoUsuarioAdmin(admin.ModelAdmin):
